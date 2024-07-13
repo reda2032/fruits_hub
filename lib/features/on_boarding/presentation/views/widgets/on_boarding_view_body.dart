54661869b865caf1d51205557890f4a9d4f46752
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-07-13 11:22:12 +0300

    Add logic part one

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -7,8 +7,31 @@ import '../../../../../constants.dart';
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/widgets/custom_button.dart';
 
-class OnBoardingViewBody extends StatelessWidget {
+class OnBoardingViewBody extends StatefulWidget {
   const OnBoardingViewBody({super.key});
+
+  @override
+  State<OnBoardingViewBody> createState() => _OnBoardingViewBodyState();
+}
+
+class _OnBoardingViewBodyState extends State<OnBoardingViewBody> {
+  late PageController pageController;
+  var currentPage = 0;
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+    pageController.addListener(() {
+      currentPage = pageController.page!.round();
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
