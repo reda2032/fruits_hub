ee03b6e8caa124f2dddb34be5f3621edf51fe8cd
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-07-13 14:16:52 +0300

    Create shared preferences singleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'core/services/shared_preferences_singleton.dart';
 import 'fruits_hub.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Prefs.init();
   runApp(const FruitsHub());
 }
