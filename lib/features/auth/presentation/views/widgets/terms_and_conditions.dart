b208923162afb9199538cf3cefd1b34233da60d2
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-07-15 21:44:06 +0300

    Signup ui part 4

--- lib/features/auth/presentation/views/widgets/terms_and_conditions.dart
+++ lib/features/auth/presentation/views/widgets/terms_and_conditions.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:fruits_hub/constants.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/custom_check_box.dart';
