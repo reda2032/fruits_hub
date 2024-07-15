48806639bd4b82a1ab97e80f6ece2ec0b69566eb
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-07-15 19:33:59 +0300

    Signup ui part 2

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../constants.dart';
+import '../../../../../core/widgets/custom_text_field.dart';
+
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+      reverse: true,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kHorizintalPadding),
+        child: Column(
+          children: [
+            const SizedBox(height: 24.0),
+            // full name
+            CustomTextFormField(
+              onSaved: (value) {},
+              hintText: 'الاسم كامل',
+              textInputType: TextInputType.name,
+            ),
+            const SizedBox(height: 16.0),
+            // email address
+            CustomTextFormField(
+              onSaved: (value) {},
+              hintText: 'البريد الإلكتروني',
+              textInputType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16.0),
+          ],
+        ),
+      ),
+    );
   }
 }
