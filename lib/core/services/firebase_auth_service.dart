81edd348a60943a8756d2a8cd5eac1947cac398d
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-07-19 17:53:18 +0300

    CImplement create user with email and password

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,1 +1,30 @@
-class FirebaseAuthService {}
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../errors/exceptions.dart';
+
+class FirebaseAuthService {
+  Future<User> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw CustomException(message: 'weak-password');
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException(
+            message: 'the account already exists for that email');
+      } else {
+        throw CustomException(message: 'An error occurred. Please try again.');
+      }
+    } catch (e) {
+      throw CustomException(message: 'An error occurred. Please try again.');
+    }
+  }
+}
