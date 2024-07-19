7208c61d03e3db771c93ea494d5a434b83a61833
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-07-19 23:12:45 +0300

    Create Bloc Observer

--- lib/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/signup_view_body.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
+import '../../../../../core/helper_functions/build_error_bar.dart';
 import '../../cubits/signup_cubits/signup_cubit.dart';
 
 class SignupViewBodyBlocConsumer extends StatelessWidget {
@@ -15,9 +16,7 @@ class SignupViewBodyBlocConsumer extends StatelessWidget {
     return BlocConsumer<SignupCubit, SignupState>(listener: (context, state) {
       if (state is SignupSuccess) {}
       if (state is SignupFailure) {
-        SnackBar(
-          content: Text(state.message),
-        );
+        buildErrorBar(context, state.message);
       }
     }, builder: (context, state) {
       return ModalProgressHUD(
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'core/services/custom_bloc_observer.dart';
 import 'core/services/get_it_service.dart';
 import 'core/services/shared_preferences_singleton.dart';
 import 'firebase_options.dart';
@@ -8,6 +10,7 @@ import 'fruits_hub.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = CustomBlocObserver();
   await Prefs.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
