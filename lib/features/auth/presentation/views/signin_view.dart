aa958c9af8131546b1d5ada18f1f1f788d667af5
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-07-20 18:05:30 +0300

    Integrate sign in cubit

--- lib/features/auth/presentation/views/signin_view.dart
+++ lib/features/auth/presentation/views/signin_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/core/services/get_it_service.dart';
 import 'package:fruits_hub/features/auth/presentation/cubits/signin_cubit/signin_cubit.dart';
-import 'package:fruits_hub/features/auth/presentation/views/widgets/signin_view_body.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/signin_view_body_bloc_consumer.dart';
 
 import '../../../../core/widgets/custom_app_bar.dart';
 import '../../domain/repos/auth_repo.dart';
@@ -20,7 +20,7 @@ class SigninView extends StatelessWidget {
       ),
       child: Scaffold(
         appBar: buildAppBar(context, title: 'تسجيل دخول'),
-        body: const SigninViewBody(),
+        body: const SigninViewBodyBlocConsumer(),
       ),
     );
   }
