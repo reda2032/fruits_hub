fb51bc43b551370f231bbacf730aec820feaa6d7
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-07-20 13:37:44 +0300

    Implement login with email

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -35,4 +35,35 @@ class FirebaseAuthService {
           message: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.');
     }
   }
+
+  Future<User> signInWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      log("Exception in FirebaseAuthService.signInWithEmailAndPassword: ${e.toString()} and code is ${e.code}");
+      if (e.code == 'user-not-found') {
+        throw CustomException(
+            message: 'الرقم السري او البريد الالكتروني غير صحيح.');
+      } else if (e.code == 'wrong-password') {
+        throw CustomException(
+            message: 'الرقم السري او البريد الالكتروني غير صحيح.');
+      } else if (e.code == 'invalid-credential') {
+        throw CustomException(
+            message: 'الرقم السري او البريد الالكتروني غير صحيح.');
+      } else if (e.code == 'network-request-failed') {
+        throw CustomException(message: 'تاكد من اتصالك بالانترنت.');
+      } else {
+        throw CustomException(
+            message: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.');
+      }
+    } catch (e) {
+      log("Exception in FirebaseAuthService.signInWithEmailAndPassword: ${e.toString()}");
+
+      throw CustomException(
+          message: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.');
+    }
+  }
 }
