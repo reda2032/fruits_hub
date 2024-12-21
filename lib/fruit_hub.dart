f4cf676a96d27853c2ca36b7e3be488630b5a7ee
reda2032 <reda.ahmed.2032@gmail.com>
2024-12-21 13:28:42 +0200

    Create Splash View

---
+++ lib/fruit_hub.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fruit Hub',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:fruits_hub/main.dart';
+import 'package:fruits_hub/fruit_hub.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const FruitHub());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
