49f7d5de016872f1553dcafe4a62486cbbfa1e62
reda2032 <reda.ahmed.2032@gmail.com>
2025-01-01 21:47:10 +0200

    Splash View ui done

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -4,7 +4,9 @@ import 'widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
+
   static const routeName = "splash";
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruits_hub/generated/assets.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(Assets.imagesPlant),
+          ],
+        ),
+        SvgPicture.asset(Assets.imagesLogo),
+        SvgPicture.asset(
+          Assets.imagesSplashBottom,
+          fit: BoxFit.fill,
+        ),
+      ],
+    );
   }
 }
