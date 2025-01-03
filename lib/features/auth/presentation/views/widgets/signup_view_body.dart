0a9d7b5e9a9b3ca7b03428bcb6890bc10b33c767
reda2032 <reda.ahmed.2032@gmail.com>
2025-01-03 21:28:08 +0200

    Signup ui part 2

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/widgets/custom_text_field.dart';
+
+import '../../../../../constants.dart';
 
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kHorizintalPadding),
+        child: Column(
+          children: [
+            const SizedBox(height: 24.0),
+            // name
+            CustomTextFormField(
+              onSaved: (value) {},
+              hintText: 'الاسم كامل',
+              textInputType: TextInputType.name,
+            ),
+            const SizedBox(height: 16.0),
+            // email
+            CustomTextFormField(
+              onSaved: (value) {},
+              hintText: 'البريد الإلكتروني',
+              textInputType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16.0),
+            // password
+            CustomTextFormField(
+              hintText: 'كلمة المرور ',
+              textInputType: TextInputType.visiblePassword,
+              obscureText: false,
+              suffixIcon: Icon(Icons.remove_red_eye),
+            ),
+            const SizedBox(height: 16.0),
+          ],
+        ),
+      ),
+    );
   }
 }
